1c69a671be24d723c0c78b958051bb272a4f5c40
Richard <richardzou25@gmail.com>
2024-10-15 06:26:13 -0400

    - views folder created - now calls index.ejs to populate port - opened viewport to convert ejs to html

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const express = require('express');
 // create app by calling express as a function
 const app = express();
+// writing our views using ejs, view engine will convert all that ejs code to html
+app.set('view engine', 'ejs')
 // creates index route for our port. req = request; res = response
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('index')
 })
 // allows us to start up our port at localhost:5000
 app.listen(5000)
