a7b7facf82f7ad8d0c24566bf16d63d5723ba23c
Richard <richardzou25@gmail.com>
2024-10-15 08:18:59 -0400

    linking articles to server and ensuring it is visible on markdown

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ app.set('view engine', 'ejs');
 app.use('/articles', articleRouter)
 // creates index route for our port. req = request; res = response
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [{
+        title: 'Test Article',
+        createdAt: Date.now(),
+        description: 'Test description'
+    }]
+    // passing all articles onto our index
+    res.render('index', { articles: articles })
 })
 // allows us to start up our port at localhost:5000
 app.listen(5000);
